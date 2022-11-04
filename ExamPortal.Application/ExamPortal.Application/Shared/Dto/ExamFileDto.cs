d59e2874e6191763806c88c9758ad792ce7fa26d
Ruan van der Merwe <ruan@colab.in.na>
2022-11-04 20:18:43 +0200

    Improvements on file upload

---
+++ ExamPortal.Application/ExamPortal.Application/Shared/Dto/ExamFileDto.cs
@@ -0,0 +1,7 @@
+namespace ExamPortal.Application.Shared.Dto;
+
+public class ExamFileDto
+{
+    public byte[] Bytes { get; set; } = new byte[] { };
+    public string FileName { get; set; } = string.Empty;
+}
